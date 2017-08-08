f270b3f412e0920fb21624db4b94a1ec7e915100
james tak <jtak93@gmail.com>
2017-08-08 13:45:38 -0700

    installs redux and react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom'
+import Home from './home'
+import About from './about'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <header>
+              <Link to="/">Home</Link>
+              <Link to="/about-us">About</Link>
+            </header>
+
+            <main>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about-us" component={About} />
+            </main>
+          </div>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
