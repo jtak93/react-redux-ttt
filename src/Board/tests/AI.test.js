f45d24ce09474256361bb1d3163a278026c5bf0d
james tak <jtak93@gmail.com>
2017-08-26 16:42:23 -0700

    fix mock data in tests to fit simpler data model

--- src/Board/tests/AI.test.js
+++ src/Board/tests/AI.test.js
@@ -2,46 +2,16 @@ import { calculateMove } from '../../modules/AI'
 
 describe('AI move calculation', () => {
   test('winning top row position 0', () => {
-    let topRow = [
-      {value: 0, isFull: false},
-      {value: 2, isFull: true},
-      {value: 2, isFull: true},
-      {value: 1, isFull: true},
-      {value: 1, isFull: true},
-      {value: 0, isFull: false},
-      {value: 1, isFull: true},
-      {value: 0, isFull: false},
-      {value: 0, isFull: false}
-    ]
+    let topRow = [null, 2, 2, 1, 1, null, 1, null, null]
     expect(calculateMove(topRow)).toBe(0)
   })
 
   test('winning left col position 0', () => {
-    let leftCol = [
-      {value: 0, isFull: false},
-      {value: 1, isFull: true},
-      {value: 1, isFull: true},
-      {value: 2, isFull: true},
-      {value: 1, isFull: true},
-      {value: 0, isFull: false},
-      {value: 2, isFull: true},
-      {value: 0, isFull: false},
-      {value: 0, isFull: false}
-    ]
+    let leftCol = [null,1,1,2,1,null,2,null,null]
     expect(calculateMove(leftCol)).toBe(0)
   })
   test('winning diag position 0', () => {
-    let diag = [
-      {value: 0, isFull: false},
-      {value: 1, isFull: true},
-      {value: 1, isFull: true},
-      {value: 1, isFull: false},
-      {value: 2, isFull: true},
-      {value: 0, isFull: false},
-      {value: 0, isFull: false},
-      {value: 0, isFull: false},
-      {value: 2, isFull: true}
-    ]
+    let diag = [null,1,1,1,2,null,null,null,2]
     expect(calculateMove(diag)).toBe(0)
   })
 })
